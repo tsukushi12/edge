5a8a55296999103a425b7470b13b5c7527d3eb86
西岡忍 <Shinobu@MacBook-Air-Shinobu.local>
2016-08-21 15:42:57 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :bbs, only: [:index, :create]
+  resources :bbs, only: :create
   root to: 'bbs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
