9e6c1a8b6cd0a33c20ff82850a75a1a0cd8db453
biohuns <biohuns@gmail.com>
2016-08-22 14:06:37 +0900

    rsync.shを更新
    sqlite3のファイル名をホスト名に変更して送るように変更

--- rsync.sh
+++ rsync.sh
@@ -3,5 +3,6 @@ RAILS_ROOT=/home/vagrant/project/edge
 RAILS_ENV=production
 DB_FILE=${RAILS_ROOT}/db/${RAILS_ENV}.sqlite3
 SSH=
+HOST=`hostname`
 
-rsync -az  "ssh -i ~.ssh/id_rsa" ${DB_FILE} ${SSH}
+rsync -az -e "ssh -i ~/.ssh/id_rsa" ${DB_FILE} ${SSH}:~/${HOST}.sqlite3
